ddad2e759f629e6ba4af30371c663b0b9a474667
Stagiaire-Ismontic <tp.stagiaire.ismontic@gmail.com>
2019-10-05 15:03:35 +0100

    nabil

--- Base de données/Section 2 - Langage SQL/Partie 2 - Langage SQL/P8- Sécurité/A82 - Exemple pratique/Exemple Simple/Mahdi Lahssini/SQLQuery1.sql
+++ Base de données/Section 2 - Langage SQL/Partie 2 - Langage SQL/P8- Sécurité/A82 - Exemple pratique/Exemple Simple/Mahdi Lahssini/SQLQuery1.sql
@@ -8,7 +8,10 @@ drop login mahdi;
 create table test1 (nom varchar(50),prenom varchar(50),age int)
 
 Grant update on test1 to mahdi with grant option;
+revoke update on test1 from mahdi;
+
 
 deny update on test1 to mahdi;
+grant update,select,delete,insert on test1 to mahdi;
+revoke update,select,delete,insert on test1 to mahdi;
 
-revoke update on test1 from mahdi;
