1be465e4a3a58c5e25b1967390ed98c30abdc4a2
Stagiaire-Ismontic <tp.stagiaire.ismontic@gmail.com>
2019-10-14 11:33:04 +0100

    oussama

--- Base de données/Section 2 - Langage SQL/Partie 3 - Programmation SQL/Groupe B/Exemple Simple/Ilias Zekri/SQLQuery1.sql
+++ Base de données/Section 2 - Langage SQL/Partie 3 - Programmation SQL/Groupe B/Exemple Simple/Ilias Zekri/SQLQuery1.sql
@@ -1,11 +1,11 @@
-DECLARE @Num1 varchar(30)='ilias'
-DECLARE @Num2 varchar(30)='ilias'
+DECLARE @Nom1 varchar(30)='ilias'
+DECLARE @Nom2 varchar(30)='ilias'
 DECLARE @Test int
 SET @Test = 1
 SELECT
 Case @Test
 WHEN 1 THEN 'Correcte'
-WHEN 2 THEN	'incorecte'
+WHEN 2 THEN 'incorecte'
 ElSE 'Error'
 END
-IF @Num1=@Num2 SELECT @Num1 ELSE SELECT @Num2
+IF @Nom1=@Nom2 SELECT @Nom1 ELSE SELECT @Nom2
--- Base de données/Section 2 - Langage SQL/Partie 3 - Programmation SQL/Groupe B/Exemple Simple/Oussama Fdili/SQLQuery1.sql
+++ Base de données/Section 2 - Langage SQL/Partie 3 - Programmation SQL/Groupe B/Exemple Simple/Oussama Fdili/SQLQuery1.sql
@@ -4,3 +4,11 @@ if @num2=@num1
 	select @num1;
 else
 	select @num2;
+DECLARE @T int
+SET @T = 1
+SELECT
+Case @Test
+WHEN 1 THEN '1'
+WHEN 2 THEN '2'
+ElSE 'Non'
+END
