f662a4935a82b95538e822f0886b3da52f924a96
Stagiaire-Ismontic <tp.stagiaire.ismontic@gmail.com>
2019-10-14 11:20:35 +0100

    ilias

--- Base de données/Section 2 - Langage SQL/Partie 3 - Programmation SQL/Groupe B/Exemple Simple/Ilias Zekri/SQLQuery1.sql
+++ Base de données/Section 2 - Langage SQL/Partie 3 - Programmation SQL/Groupe B/Exemple Simple/Ilias Zekri/SQLQuery1.sql
@@ -1,6 +1,10 @@
-declare @nom varchar(30)='ilias';
-declare @nom2 varchar(30)='zekri';
-if @nom2=@nom 
-	select @nom;
-else
-	select @nom2;
+DECLARE @Num1 varchar(30)='ilias'
+DECLARE @Num2 varchar(30)='ilias'
+DECLARE @Test int
+SET @Test = 1
+SELECT
+Case @Test
+WHEN 1 THEN IF @Num1=@Num2 SELECT @Num1 ElSE SELECT @Num2 
+WHEN 2 THEN DECLARE @Num3 varchar(30)='Zekri' SELECT @Nun3
+ElSE 'Error'
+END
