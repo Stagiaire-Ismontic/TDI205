e59107f7b5da9a01f8230b0e739d4b8b59cb041c
Stagiaire-Ismontic <tp.stagiaire.ismontic@gmail.com>
2019-10-25 09:50:56 +0100

    MOHCINE

---
+++ Base de données/S2 - Langage SQL/Pratique Langage SQL/MOHCINE TOUIL/Requete18.sql
@@ -0,0 +1,20 @@
+create database SQLSERVER;
+create table Groupes
+(
+  id int primary key,
+  nom nvarchar(30)
+);
+Create table Stagiaires
+(
+  id int,
+  nom varchar(50),
+  note_bac float,
+  filiere varchar(50),
+  id_groupe int,
+  Constraint n_note Check(note_bac between 0 and 20),
+  Check(filiere='tdi'or filiere='tri'),
+  Constraint pk_stagiaire Primary key(id),
+  Constraint fk_stagiaire_groupe Foreign key(id_groupe) References
+  Groupes(id)
+);
+select * from Stagiaires;
---
+++ Base de données/S2 - Langage SQL/Pratique Langage SQL/MOHCINE TOUIL/Requete50.sql
@@ -0,0 +1,2 @@
+
+Select Ville , max(note_bac) as NoteMax From Stagiaires GROUP BY ville ;
