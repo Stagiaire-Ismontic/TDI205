bfefab677c50e5e624fd0218d3aa94e9ed0e6364
Stagiaire-Ismontic <tp.stagiaire.ismontic@gmail.com>
2019-11-20 10:09:25 +0100

    ilias

---
+++ Programmation Client Serveur/S1.Tp/TP3/Ilias zekri/Connexion/Connexion/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Connexion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SqlConnection Con = new SqlConnection(@"Server = DESKTOP-T03IVK2\SQLEXPRESS; Database= StagiareTest; Integrated Security = true ;");
+            SqlCommand Comd;
+       
+            Con.Open();
+            Comd = new SqlCommand("INSERT INTO Test(Nom,Prenom) VALUES ('ilias','zekri')", Con);
+            Comd.ExecuteNonQuery();
+            Con.Close();
+            Console.ReadKey();
+        }
+    }
+}
