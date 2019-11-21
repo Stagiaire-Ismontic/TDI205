cd1e0d4ea87412780ad7959d9f203b5a317b9905
Stagiaire-Ismontic <tp.stagiaire.ismontic@gmail.com>
2019-11-21 16:53:46 +0100

    save

---
+++ Programmation Client Serveur/S1.Tp/TP2/Anass El Mandili/Debogage/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Debogage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 115;
+            int b = 414;
+            int result = b - a;
+
+            Console.WriteLine(result);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Programmation Client Serveur/S1.Tp/TP3/Anass El Mandili/Connexion/ConsoleApplication2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            SqlConnection cnn;
+            cnn = new SqlConnection("Data Source=DESKTOP-0QKOJIC;Initial Catalog=GestionProjets;Integrated Security=True");
+            cnn.Open();
+            SqlCommand cmd = new SqlCommand("insert into projets(id,titre=values(12,'Ahmed')", cnn);
+            cmd.ExecuteNonQuery();
+            Console.WriteLine("its Working");
+            cnn.Close();
+            Console.ReadKey();
+        }
+    }
+}
