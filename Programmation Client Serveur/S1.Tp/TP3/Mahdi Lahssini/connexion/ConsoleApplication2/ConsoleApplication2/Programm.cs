024e600a64d4bd13ae5e32ed129b8b4cf7ab5968
Stagiaire-Ismontic <tp.stagiaire.ismontic@gmail.com>
2019-11-21 17:38:00 +0100

    Mahdi

---
+++ Programmation Client Serveur/S1.Tp/TP3/Mahdi Lahssini/connexion/ConsoleApplication2/ConsoleApplication2/Programm.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            SqlConnection cnn;
+            cnn = new SqlConnection("Data Source=DESKTOP-T03IVK2;Initial Catalog=GestionProjet;Integrated Security=True");
+            cnn.Open();
+            SqlCommand cmd = new SqlCommand("insert into projet(id,titre=values(12,'Madaniii')", cnn);
+            cmd.ExecuteNonQuery();
+            Console.WriteLine("ca marche");
+            cnn.Close();
+            Console.ReadKey();
+        }
+    }
+}
