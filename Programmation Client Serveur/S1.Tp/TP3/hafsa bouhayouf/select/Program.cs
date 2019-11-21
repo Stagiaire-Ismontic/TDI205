9e5c827685d2b97f7409380f1e9c57e07e6b0148
Stagiaire-Ismontic <tp.stagiaire.ismontic@gmail.com>
2019-11-21 18:29:11 +0100

    hafsa

---
+++ Programmation Client Serveur/S1.Tp/TP3/hafsa bouhayouf/select/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace select
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SqlConnection con = new SqlConnection(@"Server=DESKTOP-T03IVK2\SQLEXPRESS;database=test;Integrated security=true;");
+            string str = "select * from Test";
+            SqlCommand cmd = new SqlCommand(str,con);
+            SqlDataReader r;
+            con.Open();
+            r = cmd.ExecuteReader();
+            while(r.Read())
+            {
+                Console.WriteLine((int)r[0]);
+                
+            }
+            con.Close();
+            Console.ReadLine();
+        }
+    }
+}
