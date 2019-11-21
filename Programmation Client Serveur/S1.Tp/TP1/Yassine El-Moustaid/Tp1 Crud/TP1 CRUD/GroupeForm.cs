ecef4ce2ac7d871dcb06df982114ef81e078422a
Stagiaire-Ismontic <tp.stagiaire.ismontic@gmail.com>
2019-11-21 17:54:39 +0100

    Yassine

---
+++ Programmation Client Serveur/S1.Tp/TP1/Yassine El-Moustaid/Tp1 Crud/TP1 CRUD/GroupeForm.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TP1_CRUD
+{
+    public partial class GroupeForm : Form
+    {
+        public GroupeForm()
+        {
+            InitializeComponent();
+        }
+        List<Groupe> lstgrp = new List<Groupe>();
+        private void btnAjtGrp_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                
+                if ((txtID.Text==null)&&(txtNDGrp.Text==null)) throw new Exception("remplir tout les champ");
+                else
+                {
+                    bool exist = false;
+                   foreach(Groupe g in lstgrp)
+                    {
+                        if (g.ID1 == int.Parse(txtID.Text)) exist = true;
+                    }
+                    if (exist) throw new Exception("ce groupe deja exist");
+                    else
+                    {
+                        lstgrp.Add(new Groupe(int.Parse(txtID.Text), txtNDGrp.Text));
+                        txtNDGrp.Text = null;
+                        txtID.Text = null;
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+    }
+}
