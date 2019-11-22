594d53ea88e9c193a72306785ef4f8092696017f
Stagiaire-Ismontic <tp.stagiaire.ismontic@gmail.com>
2019-11-22 08:59:15 +0100

    abdelilah

---
+++ Programmation Client Serveur/S1.Tp/TP4/Abdelilah El Morabit/requete_select/requete_select/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace requete_select
+{
+    class Program
+    {
+        private static void Main(string[] args, object GetInt32)
+        {
+            SqlConnection conn = new SqlConnection(@"Data Source=DESKTOP-T03IVK2\SQLEXPRESS;Initial Catalog=GestionStagiaireEtGroupe;Integrated Security=True");
+            conn.Open();
+
+            SqlCommand command = new SqlCommand("Select id,nom from Groupe",conn);
+            conn.Open();
+            SqlDataReader reader = command.ExecuteReader();
+            {
+                while (reader.Read())
+                {
+                    Console.WriteLine("{0} {1}",reader.GetInt32(0), reader.GetString(1));
+                }      
+            }
+            conn.Close();
+        }
+
+    }
+    
+}
