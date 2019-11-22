3907eb3109fb368e84f80d0103add6f74064fc13
Stagiaire-Ismontic <tp.stagiaire.ismontic@gmail.com>
2019-11-22 09:04:32 +0100

    save

---
+++ Programmation Client Serveur/S1.Tp/TP4/Anass El Mandili/Select/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Select
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connections = @"Data Source=DESKTOP-T03IVK2\SQLEXPRESS;Initial Catalog=GestionStagiaireEtGroupe;Integrated Security=True";
+            SqlConnection connection = new SqlConnection(connections);
+            using (connection)
+            {
+                SqlCommand command = new SqlCommand(
+                  "SELECT * FROM Groupe;",
+                  connection);
+                connection.Open();
+
+                SqlDataReader reader = command.ExecuteReader();
+
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        Console.WriteLine("{0}\t{1}", reader.GetInt32(0),
+                            reader.GetString(1));
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("No rows found.");
+                }
+                reader.Close();
+            }
+
+            Console.Read();
+        }
+    }
+}
