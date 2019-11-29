91d9be29f2ea4a3f10ea7361266058bc36d9730a
Stagiaire-Ismontic <tp.stagiaire.ismontic@gmail.com>
2019-11-29 11:14:47 +0100

    save

---
+++ Programmation Client Serveur/S2.Rappel/1.Linq/Groupe 1/mohamed chaer/Gr1 (Rappel C#)/Initialiseurs d'objets/Exemple/Exemple/Projets.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exemple
+{
+    public class Projets
+    {
+        private int id;
+
+        private string nom;
+
+        public int Id
+        {
+            get
+            {
+                return id;
+            }
+
+            set
+            {
+                id = value;
+            }
+        }
+        public string Nom
+        {
+            get
+            {
+                return nom;
+            }
+
+            set
+            {
+                nom = value;
+            }
+        }
+
+        public Projets()
+        {
+
+        }
+
+        public Projets(int id,string nom)
+        {
+            this.id = id;
+            this.nom = nom;
+        }
+
+    }
+}
---
+++ Programmation Client Serveur/S2.Rappel/1.Linq/Groupe 2/nabil chaouki/Linq test/Linq test/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Linq_test
+{
+    class Program
+    {
+        public delegate int Toto(int a, int b);
+        public static int somme(int a ,int b)
+        {
+            return a + b;
+
+        }
+        public static int div(int a, int b)
+        {
+            return a / b;
+
+        }
+        static void Main(string[] args)
+        {
+            Toto titi = new Toto(somme);
+           
+            Console.WriteLine("oh my good");
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine(titi(a, b)  );
+
+            titi += new Toto((x, y) => x / y);
+            Console.WriteLine(titi(a, b));
+            Console.ReadKey();
+        }
+    }
+}
