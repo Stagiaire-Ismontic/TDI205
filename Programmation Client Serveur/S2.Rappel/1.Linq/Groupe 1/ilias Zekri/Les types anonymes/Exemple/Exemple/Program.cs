b3e4b6a76ba1a4a0d81eb2cf6e92cc4049a14ee2
Stagiaire-Ismontic <tp.stagiaire.ismontic@gmail.com>
2019-11-29 09:33:07 +0100

    ilias

---
+++ Programmation Client Serveur/S2.Rappel/1.Linq/Groupe 1/ilias Zekri/Les types anonymes/Exemple/Exemple/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exemple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var v = new { id = 1, nom = "Madani" };
+        }
+    }
+}
