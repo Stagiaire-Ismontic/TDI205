4dcc44dd89b981196b25278d46e6c1f3d66b4c8a
Stagiaire-Ismontic <tp.stagiaire.ismontic@gmail.com>
2019-11-29 08:51:02 +0100

    el amoury youssra

---
+++ Programmation Client Serveur/S2.Rappel/1.Linq/Groupe 3/El amoury youssra/exemple/exemple2/exemple2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exemple2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Stagiaire> ls = new List<Stagiaire>() { new Stagiaire("madani", "el amoury") ,new Stagiaire("madani","ali")};
+            //la requete de declaration
+            var query = from s in ls where s.Nom == "madani" select s;
+            var q = query.ToList();
+            Console.WriteLine("les stagiaire dont le nom est = 'madani ' sont: ");
+            //la requete d'execution 
+            Console.WriteLine(q);
+            //foreach (Stagiaire s in ls)
+            //    {
+            //    Console.WriteLine(s.ToString());
+            //    }
+              Console.ReadLine();
+        }
+    }
+}
---
+++ Programmation Client Serveur/S2.Rappel/1.Linq/Groupe 3/El amoury youssra/exemple/exemple2/exemple2/Stagiaire.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exemple2
+{
+   public  class Stagiaire
+    {
+        private string nom;
+        private string prenom;
+
+        public string Nom
+        {
+            get
+            {
+                return nom;
+            }
+
+            set
+            {
+                nom = value;
+            }
+        }
+
+        public string Prenom
+        {
+            get
+            {
+                return prenom;
+            }
+
+            set
+            {
+                prenom = value;
+            }
+        }
+        public Stagiaire()
+        {
+
+        }
+        public Stagiaire(string nom,string prenom)
+        {
+            this.nom = nom;
+            this.Prenom = prenom;
+
+        }
+        public override string ToString()
+        {
+            string str = "le nom: "+this.Nom +","+"le prenom:"+ this.Prenom;
+            return str;
+        }
+    }
+}
