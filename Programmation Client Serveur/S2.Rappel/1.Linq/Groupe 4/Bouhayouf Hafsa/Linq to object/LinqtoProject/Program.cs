adc82e4fb61dbff52b783cfea324d0207741f87a
Stagiaire-Ismontic <tp.stagiaire.ismontic@gmail.com>
2019-11-29 09:53:17 +0100

    hafsa

---
+++ Programmation Client Serveur/S2.Rappel/1.Linq/Groupe 4/Bouhayouf Hafsa/Linq to object/LinqtoProject/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqtoProject
+{
+    class Program
+    {
+       public static void Main(string[] args)
+        {
+            List<Student> studentList = new List<Student>() {
+                new Student() { Id = 1,Nom = "JK", Age = 13} ,
+                new Student() { Id= 2, Nom = "V",  Age = 21 } ,
+                new Student() {  Id= 3, Nom = "JM",  Age = 18 } ,
+                new Student() {  Id= 4, Nom = "SG" , Age = 20} ,
+               
+            };
+
+            var teenageStudents=studentList.Where(s => s.Age > 12 && s.Age < 20);
+            Console.WriteLine("Teen age students:");
+               foreach (Student std in teenageStudents)
+            {
+                Console.WriteLine(std.Nom);
+
+            }
+            }
+
+
+        }
+    }
+}
