ff8ea63885571670eeb87aec72807de2517c9e76
Stagiaire-Ismontic <tp.stagiaire.ismontic@gmail.com>
2019-11-29 09:55:21 +0100

    save

---
+++ Programmation Client Serveur/S2.Rappel/1.Linq/Groupe 2/Anass El Mandili/DelegateConsole/DelegateConsole/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateConsole
+{
+    //Declaration Delegate
+    public delegate int Traitement(int x, int y);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Utilusation de Delegate
+            Traitement T = new Traitement(Somme);
+            Console.WriteLine(T(10,10));
+            //Expression lambda(Fonction Fléches)
+            T = new Traitement((a, b) => a + b);
+            Console.WriteLine(T(2,8));
+            //Sending lambda Expression as parametre
+            Calcul(10, 5, (a, b) => a > b);
+            Console.ReadLine();
+
+        }
+        public static int Somme(int a,int b)
+        {
+            return a + b;
+        }
+        public static void Calcul(int a, int b,Func<int,int,bool> RetBack)
+        {
+            Console.WriteLine($"Is {a} bigger than {b} :"+RetBack(a,b));
+        }
+    }
+}
---
+++ Programmation Client Serveur/S2.Rappel/1.Linq/Groupe 2/anas jalal zerhouni/delegattte/delegattte/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace delegattte
+{
+    class Program
+    {
+        public delegate int calculator(int x, int y);
+        public delegate int d1(int x, int y);
+
+        static void Main(string[] args)
+        {
+            //delegate
+            calculator c = new calculator(somme);
+            //lambda expression
+            d1 d = new d1(((a, b) => a - b));
+            //lambda expression as a parameter
+            List<string> items = new List<string> { "Adam", "By", "Cecil" };
+            //console.writeline();
+            Console.WriteLine(c(5, 2));
+            Console.WriteLine(d(10, 13));
+            printString(items, x => x.Length >= 3);
+            Console.ReadLine();
+        }
+        static int somme(int x, int y)
+        {
+            return x + y;
+        }
+        static void printString(List<string> list, Func<string, bool> lmbd)
+        {
+            var selecetedItems = list.Where(lmbd);
+            foreach (var x in selecetedItems)
+            {
+                Console.WriteLine(x);
+            }
+        }
+    }
+}
