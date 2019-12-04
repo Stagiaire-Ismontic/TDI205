9dd9a61133fca8536e94a47976ec3dc2e22550ee
Stagiaire-Ismontic <tp.stagiaire.ismontic@gmail.com>
2019-12-04 10:40:50 +0100

    anas

---
+++ Programmation Client Serveur/Tâches/A-EntityFramework/TP1/Anass EL Mandili/MyApp/MyApp/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new ServicesOptionnelEntities())
+            {
+                AddEtudiant(new Etudiant() { Id = 2, Etud_Name = "Samir",Etud_Tel="0758741258" }, context);
+                UpdateEtudiant(new Etudiant() { Id = 1, Etud_Name = "Madani", Etud_Tel = "0555545458" }, context);
+                DeleteEtudiant(context,3);
+                Console.WriteLine("\n");
+                ShowDetail(context);
+                context.SaveChanges();
+            }
+
+            Console.ReadLine();
+        }
+        public static void ShowDetail(ServicesOptionnelEntities SO)
+        {
+            foreach(Etudiant E in SO.Etudiants)
+            {
+                Console.WriteLine(E.Id+"  "+E.Etud_Name+"  "+E.Etud_Tel);
+            }
+        }
+        public static void UpdateEtudiant(Etudiant E, ServicesOptionnelEntities SO)
+        {
+            if (SO.Etudiants.Find(E.Id) != null)
+            {
+                Etudiant Et = SO.Etudiants.Find(E.Id);
+                Et.Id = E.Id;
+                Et.Etud_Name = Et.Etud_Name;
+                Et.Etud_Name = E.Etud_Name;
+                SO.SaveChanges();
+            }
+        }
+        public static void AddEtudiant(Etudiant E, ServicesOptionnelEntities context)
+        {
+            int test = 0;
+            if (context.Etudiants.Find(((Etudiant)E).Id) == null)
+            {
+                context.Etudiants.Add(E as Etudiant);
+                test = 1;
+            }        
+            context.SaveChanges();
+            if (test == 1)  
+            {
+                Console.WriteLine("Etudiant is Added a Succesfuly");
+            }
+            else Console.WriteLine("This Etudiant Is Exist! You Can't Added it");
+        }
+        public static void DeleteEtudiant(ServicesOptionnelEntities SO,int Id)
+        {
+            if(SO.Etudiants.Find(Id)!=null)
+            {
+                SO.Etudiants.Remove(SO.Etudiants.Find(Id));
+                Console.WriteLine("This one is Deleted");
+                SO.SaveChanges();
+            }
+            else
+            {
+                Console.WriteLine("This One Is Exist! You can't delete it");
+            }
+        }
+    }
+}
---
+++ Programmation Client Serveur/Tâches/A-EntityFramework/TP1/anas jalal zerhouni/EF4/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.Entity;
+
+namespace EF4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (AdminsEntities context = new AdminsEntities())
+            {
+                afficheAdmins(context);
+                Console.WriteLine("----------------------------------");
+                //AjouterAdmins(new Admins() { Id = 101, Nom = "essaa", Prenom = "small", Passwsord = "4959875416" }, context);
+                //AjouterAdmins(new Admins() { Id = 102, Nom = "esa", Prenom = "gra", Passwsord = "495545416" }, context);
+                //afficheAdmins(context);
+                Console.WriteLine("----------------------------------");
+                //deleteAdmin(102, context);
+                //afficheAdmins(context);
+                Console.WriteLine("---------------------------------------");
+                UpdateAdmins(new Admins() { Id = 101, Nom = "brahim", Prenom = "brahim", Passwsord = "123456" },context);
+                afficheAdmins(context);
+                Console.ReadLine();
+
+            }
+
+        }
+        public static void afficheAdmins(AdminsEntities context)
+        {
+            foreach (Admins a in context.Admins)
+            {
+                Console.WriteLine(a.Id + " " + a.Nom + "  " + a.Prenom + " " + a.Passwsord );
+            }
+        }
+
+        public static void AjouterAdmins(Admins a, AdminsEntities context)
+        {
+            if (context.Admins.Find(a.Id) == null)
+            {
+                context.Admins.Add(a);
+                Console.WriteLine("Done");
+                context.SaveChanges();
+            }
+            else
+            {
+                Console.WriteLine("this id doesnt exist");
+            }
+        }
+
+        public static void deleteAdmin(int id, AdminsEntities context)
+        {
+            if (context.Admins.Find(id) != null)
+            {
+                context.Admins.Remove(context.Admins.Find(id));
+                Console.WriteLine("Done");
+                context.SaveChanges();
+            }
+            else
+            {
+                Console.WriteLine("Id doesnt exist");
+            }
+        }
+
+        public static void UpdateAdmins(Admins a, AdminsEntities context)
+        {
+            if (context.Admins.Find(a.Id) != null)
+            {
+                Admins aa = context.Admins.Where(aaa => a.Id == aaa.Id).FirstOrDefault();
+                aa.Nom = a.Nom;
+                aa.Prenom = a.Prenom;
+                aa.Passwsord = a.Passwsord;
+                Console.WriteLine("Done");
+                context.SaveChanges();
+            }
+            else
+            {
+                Console.WriteLine("Id doesnt exist");
+            }
+        }
+
+    }
+}
