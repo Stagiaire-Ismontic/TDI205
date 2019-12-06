01e48d2fb39a2c0083be0e9d56c29c376325694c
Stagiaire-Ismontic <tp.stagiaire.ismontic@gmail.com>
2019-12-06 10:22:07 +0100

    imane

---
+++ Programmation Client Serveur/Tâches/A-EntityFramework/TP2/Imane Amro/tp2/tp2/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tp2
+{
+    public partial class Employer
+    {
+        public int Id { get; set; }
+        public string Nom { get; set; }
+        public string prenom { get; set; }
+        public Nullable<int> IdEntreprise { get; set; }
+
+        public virtual Entreprise Entreprise { get; set; }
+    }
+
+}
