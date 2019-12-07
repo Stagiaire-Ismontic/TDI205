d71ba672dd01e2f55eed5d885b7bf758c4b762b7
Stagiaire-Ismontic <tp.stagiaire.ismontic@gmail.com>
2019-12-07 18:16:22 +0100

    omar

---
+++ PFF/1- Analyse technique/CRUD windows/omar ben sabih el imrany/GestionGroupe/Personne.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionGroupe
+{
+    class Personne
+    {
+        private int id;
+        public int Id
+        {
+            get
+            {
+                return id;
+            }
+
+            set
+            {
+                id = value;
+            }
+        }
+
+        private string nom;
+        public string Nom
+        {
+            get
+            {
+                return nom;
+            }
+
+            set
+            {
+                nom = value;
+            }
+        }
+        private int age;
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+
+            set
+            {
+                age = value;
+            }
+        }
+
+    }
+}
