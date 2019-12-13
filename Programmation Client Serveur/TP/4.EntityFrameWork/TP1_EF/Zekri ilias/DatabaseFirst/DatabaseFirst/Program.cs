717a0000302f3d41f11f82a07ad3ceb10ca8cb41
Stagiaire-Ismontic <tp.stagiaire.ismontic@gmail.com>
2019-12-13 11:41:40 +0100

    ilias

--- Programmation Client Serveur/TP/4.EntityFrameWork/TP1_EF/Zekri ilias/DatabaseFirst/DatabaseFirst/Program.cs
+++ Programmation Client Serveur/TP/4.EntityFrameWork/TP1_EF/Zekri ilias/DatabaseFirst/DatabaseFirst/Program.cs
@@ -4,34 +4,45 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DatabaseFirst
+namespace DataBaseFirst
 {
     class Program
     {
         static void Main(string[] args)
         {
-            using (var context = new GestionControleEntities())
+            using (var c=new GestionComEntities())
             {
-
                 ////Ajouter
-                //context.Controles.Add(new Controle() { Id = 1, Nom = "ilias", Note = 15 });
-                //context.SaveChanges();
-
-                ////Supprimer
-                //Controle C = context.Controles.SingleOrDefault(s => s.Id == 1);
-                //context.Controles.Remove(C);
-                //context.SaveChanges();
+                //Produit p = new Produit() { IdProduit = 1, Nom = "Kawkaw", Prix = 50 };
+                //c.Produits.Add(p);
+                //Commande Cm = new Commande() {IdCommande=1,Descriptions="Commandes",Prix=4000,Produit=p,ProduitId=p.IdProduit };
+                //c.Commandes.Add(Cm);
+                //c.SaveChanges();
 
                 ////Modifier
-                //Controle C = context.Controles.SingleOrDefault(s => s.Id == 1);
-                //C.Nom = "Zekri";
-                //C.Note = 17;
-                //context.SaveChanges();
+                //Produit P= c.Produits.SingleOrDefault(s => s.IdProduit == 1);
+                //P.Nom = "K";
+                //P.Prix = 6;
+                //Commande Cm = c.Commandes.SingleOrDefault(s => s.IdCommande == 1);
+                //Cm.Descriptions = "Free";
+                //Cm.Prix = 1200;
+                //c.SaveChanges();
+
+                ////Supprimer
+                //Produit P = c.Produits.SingleOrDefault(s => s.IdProduit == 1);
+                //c.Produits.Remove(P);
+                //Commande Cm = c.Commandes.SingleOrDefault(s => s.IdCommande == 1);
+                //c.Commandes.Remove(Cm);
+                //c.SaveChanges();
 
                 ////Afficher
-                //foreach (var item in context.Controles.ToList())
+                //foreach (var item in c.Produits.ToList())
                 //{
-                //    Console.WriteLine(item.Id + "  " + item.Nom+"  "+item.Note);
+                //    Console.WriteLine(item.IdProduit+"  "+item.Nom);
+                //}
+                //foreach (var item in c.Commandes.ToList())
+                //{
+                //    Console.WriteLine(item.IdCommande + "  " + item.Descriptions);
                 //}
 
             }
--- Programmation Client Serveur/TP/4.EntityFrameWork/TP1_EF/Zekri ilias/DatabaseFirst/SQLQuery.sql
+++ Programmation Client Serveur/TP/4.EntityFrameWork/TP1_EF/Zekri ilias/DatabaseFirst/SQLQuery.sql
@@ -1,4 +1,4 @@
-Create database GestionControle;
-use GestionControle
-Create table Controle(Id int primary key,Nom varchar(50),Note float); 
-select * from Controle;
+Create database GestionCom;
+use GestionCom
+Create table Produit (IdProduit int primary key,Nom varchar(50),Prix money);
+Create table Commande (IdCommande int primary key,Descriptions varchar(50),Prix money,ProduitId int ,constraint fk foreign key (ProduitId) references Produit(IdProduit));
