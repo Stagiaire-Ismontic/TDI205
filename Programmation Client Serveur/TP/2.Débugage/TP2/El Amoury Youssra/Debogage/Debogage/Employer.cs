36bc4d2063a924e7eeae2f9e91280b12daee35dc
Stagiaire-Ismontic <tp.stagiaire.ismontic@gmail.com>
2019-12-18 10:24:11 +0100

    Youssra

---
+++ Programmation Client Serveur/TP/2.Débugage/TP2/El Amoury Youssra/Debogage/Debogage/Employer.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Debogage
+{
+  public   class Employer
+    {
+        private int id;
+        private string nom;
+        private string prenom;
+
+        public int Id
+        {
+            get
+            {
+                return id;
+            }
+
+            set
+            {
+                id = value;
+            }
+        }
+
+        public string Nom
+        {
+            get
+            {
+                return nom;
+            }
+
+            set
+            {
+                nom = value;
+            }
+        }
+
+        public string Prenom
+        {
+            get
+            {
+                return prenom;
+            }
+
+            set
+            {
+                prenom = value;
+            }
+        }
+        public Employer(int id,string nom,string prenom)
+        {
+            this.id = id;
+            this.nom = nom;
+            this.prenom = prenom;
+        }
+        public override string ToString()
+        {
+            string str = "id= " + this.id + ", le nom= " + this.nom + " ,le prenom= " + this.prenom;
+            return str;
+        }
+    }
+}
---
+++ Programmation Client Serveur/TP/2.Débugage/TP2/El Amoury Youssra/Debogage/Debogage/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Debogage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employer e1 = new Employer(1, "madani", "ali");
+            Employer e2 = new Employer(2, "youssra", "amoury");
+            Employer e3 = new Employer(3, "loubna", "anouja");
+            Employer e4 = new Employer(4, "imane", "amro");
+            Console.WriteLine("le prenom de employer 2 est:");
+            Console.WriteLine(e1.Prenom);
+            Console.WriteLine(e2.ToString());
+
+            Console.ReadLine();
+
+        }
+    }
+}
